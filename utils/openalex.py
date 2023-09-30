edd267753cf4b48eed3c70da25278a36ec997497
吴南李 <wunanli2002@163.com>
2023-09-30 17:20:33 +0800

    完成作者搜索

--- utils/openalex.py
+++ utils/openalex.py
@@ -1,14 +1,16 @@
-from pyalex import Works
+from pyalex import Works, Authors
 from .cache import get_openalex_entities_cache, set_openalex_entities_cache
 
-def search_works(search:str, filter:dict=None, sort:dict=None, page:int=1, size:int=25):
+
+def search_works(search: str, filter: dict = None, sort: dict = None, page: int = 1, size: int = 25):
     if filter is None:
         filter = {}
     if sort is None:
         sort = {}
     result = get_openalex_entities_cache('work', search, filter, sort, page, size)
     if result is None:
-        result,meta = Works().search(search).filter(**filter).sort(**sort).get(return_meta=True, page=page, per_page=size)
+        result, meta = Works().search(search).filter(**filter).sort(**sort).get(return_meta=True, page=page,
+                                                                                per_page=size)
         for r in result:
             if r.get('abstract_inverted_index') is not None:
                 r['abstract'] = r['abstract']
@@ -22,4 +24,23 @@ def search_works(search:str, filter:dict=None, sort:dict=None, page:int=1, size:
             'result': result
         }
         set_openalex_entities_cache(result, 'work', search, filter, sort, page, size)
+    return result
+
+
+def search_authors(search: str, filter: dict = None, sort: dict = None, page: int = 1, size: int = 25):
+    if filter is None:
+        filter = {}
+    if sort is None:
+        sort = {}
+    result = get_openalex_entities_cache('author', search, filter, sort, page, size)
+    if result is None:
+        result, meta = Authors().search(search).filter(**filter).sort(**sort).get(return_meta=True, page=page,
+                                                                                  per_page=size)
+        result = {
+            'total': meta['count'],
+            'page': meta['page'],
+            'size': meta['per_page'],
+            'result': result
+        }
+        set_openalex_entities_cache(result, 'author', search, filter, sort, page, size)
     return result
