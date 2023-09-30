a10ee305a5d632d73a96aa4ecbc98ee5f9767872
吴南李 <wunanli2002@163.com>
2023-09-30 17:48:24 +0800

    修改方法为使用装饰器

--- author/views.py
+++ author/views.py
@@ -23,11 +23,6 @@ def search_author_view(request):
 
 @request_methods(['POST'])
 def author_detail_view(request):
-    if request.method != 'POST':
-        return JsonResponse({
-            'success': False,
-            'message': '不允许的方法'
-        })
     data = json.loads(request.body)
     id = data.get('id')
     if not id:
