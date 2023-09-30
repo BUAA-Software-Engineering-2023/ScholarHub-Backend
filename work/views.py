4e43d3c5badb7db5e4864b9d8a9655151c6e62e4
BienBoy <285915362@qq.com>
2023-09-30 16:53:49 +0800

    完成论文搜索

--- work/views.py
+++ work/views.py
@@ -1,6 +1,22 @@
 import json
 
 from django.http.response import JsonResponse
+from utils.openalex import search_works
 
 def search_work_view(request):
-    if (request.method != 'GET')
+    if request.method != 'POST':
+        return JsonResponse({
+            'success': False,
+            'message': '不允许的方法'
+        })
+    data = json.loads(request.body)
+    search = data.get('search', '')
+    filter = data.get('filter')
+    sort = data.get('sort')
+    page = int(data.get('page'))
+    size = int(data.get('size'))
+    result = search_works(search, filter, sort, page, size)
+    return JsonResponse({
+        'success': True,
+        'data': result
+    })
