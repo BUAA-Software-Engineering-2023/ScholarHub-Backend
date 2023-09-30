4abbb6a1700ff07c5fbba6811638f31dab579727
BienBoy <285915362@qq.com>
2023-09-30 17:23:15 +0800

    完成获取论文详情

--- utils/openalex.py
+++ utils/openalex.py
@@ -23,3 +23,15 @@ def search_works(search:str, filter:dict=None, sort:dict=None, page:int=1, size:
         }
         set_openalex_entities_cache(result, 'work', search, filter, sort, page, size)
     return result
+
+def get_single_work(id:str):
+    try:
+        result = Works()[id]
+    except:
+        return None
+    if result.get('abstract_inverted_index') is not None:
+        result['abstract'] = result['abstract']
+        del result['abstract_inverted_index']
+    else:
+        result['abstract'] = ''
+    return result
--- work/views.py
+++ work/views.py
@@ -1,9 +1,9 @@
 import json
 
 from django.http.response import JsonResponse
-from utils.openalex import search_works
+from utils.openalex import search_works, get_single_work
 
-def search_work_view(request):
+def search_works_view(request):
     if request.method != 'POST':
         return JsonResponse({
             'success': False,
@@ -13,9 +13,33 @@ def search_work_view(request):
     search = data.get('search', '')
     filter = data.get('filter')
     sort = data.get('sort')
-    page = int(data.get('page'))
-    size = int(data.get('size'))
+    page = int(data.get('page', 1))
+    size = int(data.get('size', 25))
     result = search_works(search, filter, sort, page, size)
+    return JsonResponse({
+        'success': True,
+        'data': result
+    })
+
+def work_detail_view(request):
+    if request.method != 'POST':
+        return JsonResponse({
+            'success': False,
+            'message': '不允许的方法'
+        })
+    data = json.loads(request.body)
+    id = data.get('id')
+    if not id:
+        return JsonResponse({
+            'success': False,
+            'message': '请给出id'
+        })
+    result = get_single_work(id)
+    if not result:
+        return JsonResponse({
+            'success': False,
+            'message': '不存在的论文'
+        })
     return JsonResponse({
         'success': True,
         'data': result
