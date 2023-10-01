9512755ae5d483214a228518604c7059b82e7b9a
BienBoy <285915362@qq.com>
2023-10-01 21:26:07 +0800

    使用celery发送邮件

--- user/views.py
+++ user/views.py
@@ -6,13 +6,12 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.core.exceptions import ValidationError
 from django.core.validators import EmailValidator
 from django.http.response import JsonResponse
-from django.template import loader
-from django.core.mail import EmailMessage
 
 from user.models import User
 from utils.cache import get_verification_code_cache, set_verification_code_cache
 from utils.decorator import request_methods
 from utils.token import make_token
+from .tasks import celery_send_verification_code
 
 
 @request_methods(['POST'])
@@ -33,24 +32,14 @@ def send_verification_code_view(request):
     # 写入缓存
     set_verification_code_cache(email, code)
 
-    # TODO 使用celery处理发送邮件
-    t = loader.get_template('verification_code.html')
-    html = t.render({'code': code})
-    # 发送邮件
-    msg = EmailMessage('ScholarHub邮箱验证码', html,
-                       to=[email])
-    msg.content_subtype = 'html'
-    send_status = msg.send()
-    if not send_status:
-        return JsonResponse({
-            'success': False,
-            'message': '验证码发送失败，请稍后重试'
-        })
+    celery_send_verification_code.delay(email, code)
+
     # TODO 最终需移除返回的验证码
     return JsonResponse({
         'success': True,
         'code': code
     })
+
 
 @request_methods(['POST'])
 def register_view(request):
