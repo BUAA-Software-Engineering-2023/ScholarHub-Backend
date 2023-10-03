a35a03ad590c99b0e4e7bcf255d7b028d1c853af
BienBoy <285915362@qq.com>
2023-10-04 00:36:50 +0800

    增加中间件处理token

--- ScholarHub/settings.py
+++ ScholarHub/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'utils.token.TokenMiddleware'
 ]
 
 ROOT_URLCONF = 'ScholarHub.urls'
--- utils/token.py
+++ utils/token.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from user.models import User
 
 
-def make_token(data:dict, expire:int=30*24*3600)->str:
+def make_token(data: dict, expire: int = 30 * 24 * 3600) -> str:
     """
     根据给数据生成token
 
@@ -22,39 +22,56 @@ def make_token(data:dict, expire:int=30*24*3600)->str:
     }
     return jwt.encode(payload, key)
 
+
+def parse_token(token):
+    if not token:
+        return None, 403
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+    except jwt.DecodeError:
+        return None, 401
+    try:
+        user = User.objects.get(username=payload['username'])
+    except User.DoesNotExist:
+        return None, 401
+    except Exception as e:
+        print(f'意外报错：{e.args}')
+        return None, 200
+    return user, 200
+
+
 def auth_check(func):
     """
     包装器，用于在调用函数前通过校验token判断是否登录
     :param func:
     :return:
     """
+
     def wrap(request, *args, **kwargs):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        if not token:
-            return JsonResponse({
-                'success': False,
-                'message': '请先进行登录'
-            }, status=403)
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
-        except jwt.DecodeError:
-            return JsonResponse({
-                'success': False,
-                'message': '无效的token'
-            }, status=401)
-        try:
-            user = User.objects.get(username=payload['username'])
-        except User.DoesNotExist:
-            return JsonResponse({
-                'success': False,
-                'message': '无效的token'
-            }, status=401)
-        except Exception as e:
-            print(f'意外报错：{e.args}')
-            return JsonResponse({
-                'success': False,
-                'message': '未知错误'
-            })
-        request.user = user
+        if not request.user:
+            if request.token_status == 403:
+                return JsonResponse({
+                    'success': False,
+                    'message': '请先进行登录'
+                }, status=403)
+            if request.token_status == 403:
+                return JsonResponse({
+                    'success': False,
+                    'message': '未知错误'
+                })
         return func(request, *args, **kwargs)
+
     return wrap
+
+
+class TokenMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        token = request.META.get('HTTP_AUTHORIZATION')
+        request.user, request.token_status = parse_token(token)
+
+        response = self.get_response(request)
+
+        return response
