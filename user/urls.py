97cfaf7c66a75c92fc57445bfd66d36dd7adb0ad
吴南李 <wunanli2002@163.com>
2023-10-22 23:15:12 +0800

    重构提问区相关接口
    重构提问区相关接口
    为用户添加头像
    重构用户信息更新接口

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('send-code', send_verification_code_view),
     path('register', register_view),
     path('login', login_view),
+    path('user/update_email', update_email_view),
+    path('user/update_password', update_password_view),
     path('user/update', update_userinfo_view),
     path('user/retrieve', retrieve_password_view),
     path('user/info', get_userinfo_view)
